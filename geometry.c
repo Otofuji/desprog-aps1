20f17a7a75f54eb0316d7aea61cda272e0b16ce2
Eric Fernando Otofuji Abrantes <ericfoa@al.insper.edu.br>
2020-03-05 15:20:33 -0300

    quase

--- geometry.c
+++ geometry.c
@@ -17,17 +17,36 @@ int verify(point p, point a, point b) {
   double by = (double)b.y;
   double px = (double)p.x;
   double py = (double)p.y;
+  
   double mr = (by-ay)/(bx-ax);
   double mp = (py-ay)/(px-ax);
+  double xp = (py-ay)/mr;
+  double xd = px - xp;
+
+  point d;
+  int infill = 0;
+  
 
   if (  (distance(ax, ay, px, py) + distance(bx, by, px, py) == distance(ax, ay, bx, by))  &&  ((mr == mp) && ((ax != px) && (ay != py)) && ((bx != px) && (by != py))  ))  {return 2;} //https://stackoverflow.com/questions/17692922/check-is-a-point-x-y-is-between-two-points-drawn-on-a-straight-line
   if (  (distance(ax, ay, px, py) + distance(bx, by, px, py) == distance(ax, ay, bx, by))  &&  (              ((ax == px) && (ay == py)) || ((bx == px) && (by == py))  ))  {return 2;}
   if (  (distance(ax, ay, px, py) + distance(bx, by, px, py) == distance(ax, ay, bx, by))  &&  (              (       (ax == bx)       ) || (       (ay == by)       )  ))  {return 2;}
 
-  if ((ay - by <= zero)  &&  ((ay < py) && (py <= by))    &&    (ax - bx <= zero)  &&  (px <= ax)) {return 1;}
-  if ((ay - by <= zero)  &&  ((ay < py) && (py <= by))    &&    (bx - ax <= zero)  &&  (px <= bx)) {return 1;}
-  if ((by - ay <= zero)  &&  ((by < py) && (py <= ay))    &&    (bx - ax <= zero)  &&  (px <= bx)) {return 1;}
-  if ((by - ay <= zero)  &&  ((by < py) && (py <= ay))    &&    (ax - bx <= zero)  &&  (px <= ax)) {return 1;}
+  if (  (distance(ax, ay, xd, py) + distance(bx, by, xd, py) == distance(ax, ay, bx, by))  &&  ((mr == mp) && ((ax != xd) && (ay != xd)) && ((bx != xd) && (by != py))  ))  {infill = 1;}
+  if (  (distance(ax, ay, xd, py) + distance(bx, by, xd, py) == distance(ax, ay, bx, by))  &&  (              ((ax == xd) && (ay == xd)) || ((bx == xd) && (by == py))  ))  {infill = 1;}
+  if (  (distance(ax, ay, xd, py) + distance(bx, by, xd, py) == distance(ax, ay, bx, by))  &&  (              (       (ax == bx)       ) || (       (ay == by)       )  ))  {infill = 1;}
+  
+  
+
+  if ((ay < by)  &&  ((ay < py) && (py > by))    &&      ((px <= xd) && (fabs(ax-bx) > zero))   &&   (infill == 1)) {return 1;}
+  if ((by < by)  &&  ((by < py) && (py > ay))    &&      ((px <= xd) && (fabs(ax-bx) > zero))   &&   (infill == 1)) {return 1;}
+
+  if (fabs(ax - bx) <= zero && ((ay < by && (fabs(ax-bx) > zero)) || (by < ay && (fabs(ax-bx) > zero)))) {return 1;}
+
+
+  //if ((ay - by <= zero)  &&  ((ay < py) && (py > by))    &&    (ax - bx <= zero)  &&  (px <= ax)) {return 1;}
+  //if ((ay - by <= zero)  &&  ((ay < py) && (py > by))    &&    (bx - ax <= zero)  &&  (px <= bx)) {return 1;}
+  //if ((by - ay <= zero)  &&  ((by < py) && (py > ay))    &&    (bx - ax <= zero)  &&  (px <= bx)) {return 1;}
+  //if ((by - ay <= zero)  &&  ((by < py) && (py > ay))    &&    (ax - bx <= zero)  &&  (px <= ax)) {return 1;}
 
   return 0;
 }
