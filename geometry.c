07ee24db6f7ecdc174b2a57fdd25a86ec6b39df3
Eric Fernando Otofuji Abrantes <ericfoa@al.insper.edu.br>
2020-03-05 15:48:23 -0300

    Implementando resultados nos testes de unidade

--- geometry.c
+++ geometry.c
@@ -3,7 +3,7 @@
 #include "geometry.h"
 
 double distance(double ax, double ay, double bx, double by) {
-  double dist = 0;
+  double dist = 0.0;
   dist += fabs(bx-ax); //https://stackoverflow.com/questions/20956352/how-to-get-absolute-value-from-double-c-language
   dist += fabs(by-ay);
   return dist;
@@ -17,25 +17,25 @@ int verify(point p, point a, point b) {
   double by = (double)b.y;
   double px = (double)p.x;
   double py = (double)p.y;
-  
   double mr = (by-ay)/(bx-ax);
   double mp = (py-ay)/(px-ax);
   double xp = (py-ay)/mr;
   double xd = px - xp;
-  int infill = 0;
+  int    il = 0;
   
 
-  if (  (fabs((distance(ax, ay, px, py) + distance(bx, by, px, py)) - (distance(ax, ay, bx, by))) <= zero)  &&  ((fabs(mr-mp)<=zero) && ((fabs(ax-px)>zero) && (fabs(ay-py)>zero)) && ((fabs(bx-px)>zero) && (fabs(by-py)>zero))  ))  {return 2;} //https://stackoverflow.com/questions/17692922/check-is-a-point-x-y-is-between-two-points-drawn-on-a-straight-line
-  if (  (fabs((distance(ax, ay, px, py) + distance(bx, by, px, py)) - (distance(ax, ay, bx, by))) <= zero)  &&  (              ((fabs(ax-px)<=zero) && (fabs(ay-py)<=zero)) || ((fabs(bx-px)<=zero) && (fabs(by-py)<=zero))  ))  {return 2;}
-  if (  (fabs((distance(ax, ay, px, py) + distance(bx, by, px, py)) - (distance(ax, ay, bx, by))) <= zero)  &&  (              (       (fabs(ax-bx)<=zero)       ) || (       (fabs(ay-by)<=zero)       )  ))  {return 2;}
-  if (  (fabs((distance(ax, ay, xd, py) + distance(bx, by, xd, py)) - (distance(ax, ay, bx, by))) <= zero)  &&  ((fabs(mr-mp)<=zero) && ((fabs(ax-xd)>zero) && (fabs(ay-xd)>zero)) && ((fabs(bx-xd)>zero) && (fabs(by-py)>zero))  ))  {infill = 1;}
-  if (  (fabs((distance(ax, ay, xd, py) + distance(bx, by, xd, py)) - (distance(ax, ay, bx, by))) <= zero)  &&  (              ((fabs(ax-xd)<=zero) && (fabs(ay-xd)<=zero)) || ((fabs(bx-xd)<=zero) && (fabs(by-py)<=zero))  ))  {infill = 1;}
-  if (  (fabs((distance(ax, ay, xd, py) + distance(bx, by, xd, py)) - (distance(ax, ay, bx, by))) <= zero)  &&  (              (       (fabs(ax-bx)<=zero)       ) || (       (fabs(ay-by)<=zero)       )  ))  {infill = 1;}
-  
+  if (  (fabs((distance(ax, ay, px, py) + distance(bx, by, px, py)) - (distance(ax, ay, bx, by))) <= zero)  &&  ((fabs(mr-mp) <= zero) && ((fabs(ax-px) > zero) && (fabs(ay-py) >  zero)) && ((fabs(bx-px) >  zero) && (fabs(by-py) >  zero))  ))  {return 2;} //https://stackoverflow.com/questions/17692922/check-is-a-point-x-y-is-between-two-points-drawn-on-a-straight-line
+  if (  (fabs((distance(ax, ay, px, py) + distance(bx, by, px, py)) - (distance(ax, ay, bx, by))) <= zero)  &&  (              (   (fabs(ax-px) <= zero)      && (fabs(ay-py) <= zero))   || ((fabs(bx-px) <= zero) && (fabs(by-py) <= zero))  ))  {return 2;}
+  if (  (fabs((distance(ax, ay, px, py) + distance(bx, by, px, py)) - (distance(ax, ay, bx, by))) <= zero)  &&  (              (                  (fabs(ax-bx)<=zero)                   ) || (               (fabs(ay-by)<=zero)            )  ))  {return 2;}
   
 
-  if ((ay < by)  &&  ((ay < py) && (py > by))    &&      ((px <= xd) && (fabs(ax-bx) > zero))   &&   (infill == 1)) {return 1;}
-  if ((by < by)  &&  ((by < py) && (py > ay))    &&      ((px <= xd) && (fabs(ax-bx) > zero))   &&   (infill == 1)) {return 1;}
+
+  if (  (fabs((distance(ax, ay, xd, py) + distance(bx, by, xd, py)) - (distance(ax, ay, bx, by))) <= zero)  &&  ((fabs(mr-mp) <= zero) && ((fabs(ax-xd) > zero) && (fabs(ay-xd) >  zero)) && ((fabs(bx-xd) >  zero) && (fabs(by-py) >  zero))  ))  {il = 1;}
+  if (  (fabs((distance(ax, ay, xd, py) + distance(bx, by, xd, py)) - (distance(ax, ay, bx, by))) <= zero)  &&  (              (    (fabs(ax-xd)<=zero)       && (fabs(ay-xd)<=zero))     || ((fabs(bx-xd) <= zero) && (fabs(by-py) <= zero))  ))  {il = 1;}
+  if (  (fabs((distance(ax, ay, xd, py) + distance(bx, by, xd, py)) - (distance(ax, ay, bx, by))) <= zero)  &&  (              (                  (fabs(ax-bx)<=zero)                   ) || (               (fabs(ay-by)<=zero)            )  ))  {il = 1;}
+  
+  if ((ay < by)  &&  ((ay < py) && (py > by))    &&      ((px <= xd) && (fabs(ax-bx) > zero))   &&   (il == 1)) {return 1;}
+  if ((by < by)  &&  ((by < py) && (py > ay))    &&      ((px <= xd) && (fabs(ax-bx) > zero))   &&   (il == 1)) {return 1;}
 
   if ((fabs(ax - bx) <= zero) && (((ay < by) && (fabs(ax-bx) > zero)) || ((by < ay) && (fabs(ax-bx) > zero)))) {return 1;}
 
